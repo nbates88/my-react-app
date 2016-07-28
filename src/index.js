8885e5e195a0df2579054e3a660f66b67c4c8e29
Nichole Bates <nbates88@gmail.com>
2016-07-28 16:33:53 -0400

    experimenting with state

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import ProductList from './App';
+import App from './js/components/App';
 import './index.css';
 
 ReactDOM.render(
-  <ProductList />,
+  <App />,
   document.getElementById('root')
 );
---
+++ src/js/components/App.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react';
+import Footer from './Footer';
+import Header from './Header';
+import '../../App.css';
+
+//everything in react is a component. each component returns exactly one DOM element. To return multiple, wrap in div
+//default of state is null
+//manages virtual DOM. If there are changes in DOM, it will update only relevant nodes
+//state only gets used if component has internal value that only affects that component. Else, use props
+class App extends Component {
+	constructor() {
+		super();
+		this.state = {name: 'Nichole'}
+	}
+	render() {
+		setTimeout(() => {
+			this.setState({name: "Bob"})
+		}, 1000)
+    return (
+	    <div>
+		    {this.state.name}
+		    <Header />
+		    <Footer />
+	    </div>
+    );
+  }
+}
+
+export default App;
+
